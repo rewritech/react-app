b7cd07dfa9e7d65db6ce201da67bfcd48aca0dca
GHJ <rwtstar21@gmail.com>
2020-01-05 01:17:56 +0900

    Uadate css clean

--- src/App.js
+++ src/App.js
@@ -6,10 +6,7 @@ class App extends Component {
   render() {
     return (
       <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          Hello, React!!
-        </header>
+        Hello, React!!
       </div>
     );
   }
